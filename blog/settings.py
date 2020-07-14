899e8857a468b57eb5e767c6e55b4396169aadbd
jaylalakiya <63226109+jaylalakiya@users.noreply.github.com>
2020-07-14 17:06:57 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1wcb&qxj6iec4ek$v=^jq$p3!64u^jhypm$2t$nlozyirarmtb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'jaylalakiya.pythonanywhere.com']
 
