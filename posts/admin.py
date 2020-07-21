fa3e7fded6eaec573f7c4f60883f25c86d1373ef
jaylalakiya <63226109+jaylalakiya@users.noreply.github.com>
2020-07-21 17:04:26 +0530

    improved admin.py Post model look
    added more features in Post model of admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from .models import Author, Category, Post, News
 
+
+# registering 'Author' model
 admin.site.register(Author)
+
+
+# registering 'Category' model
 admin.site.register(Category)
-admin.site.register(Post)
+
+
+# registering 'Post' model
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'timestamp', 'author', 'featured')
+    list_filter = ('categories', 'featured', 'timestamp', 'author')
+    raw_id_fields = ('author')
+    ordering = ('timestamp')
+
+
+# registering 'News' model
 admin.site.register(News)
