ede6b6d127a9ba5b91ec63dc6b49efc5e3909575
jaylalakiya <63226109+jaylalakiya@users.noreply.github.com>
2020-07-21 17:07:10 +0530

    Update admin.py
    fixed issue

--- posts/admin.py
+++ posts/admin.py
@@ -16,8 +16,8 @@ admin.site.register(Category)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'timestamp', 'author', 'featured')
     list_filter = ('categories', 'featured', 'timestamp', 'author')
-    raw_id_fields = ('author')
-    ordering = ('timestamp')
+    raw_id_fields = ('author',)
+    ordering = ('timestamp',)
 
 
 # registering 'News' model
